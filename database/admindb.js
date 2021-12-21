1543295d752bfa196cf5bc856542b17baa18ba03
karthick587 <85030277+karthick587@users.noreply.github.com>
2021-12-21 21:31:12 +0530

    Update admindb.js

--- database/admindb.js
+++ database/admindb.js
@@ -10,10 +10,10 @@ app.use(express.json());
 app.use(cors());
 
 const con=mysql.createConnection({
-    host:"sql305.epizy.com",
-    user:"epiz_30606693",
-    password:'K0Z7LZmjqddGje0',
-    database:"epiz_30606693_customerdb"
+    host:"localhost",
+    user:"root",
+    password:'',
+    database:"customerdb"
 });
 
 con.connect((err) =>{
