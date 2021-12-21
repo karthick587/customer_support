f2924963a5c14dfd62bae1957f362ef1b17cd291
karthick587 <85030277+karthick587@users.noreply.github.com>
2021-12-21 17:54:49 +0530

    Update admindb.js

--- database/admindb.js
+++ database/admindb.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://karthickraja:Ben12345#@cluster0.h7hwq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://karthickraja:<Ben12345#>@cluster0.h7hwq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
