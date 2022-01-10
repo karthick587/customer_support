bfae306ccf0d672aad41b6c6602b2b897cca7a3e
admin <karthickdurai587@gmail.com>
2022-01-10 11:42:41 +0530

    all

--- pages/components/tickets/adminticket.js
+++ pages/components/tickets/adminticket.js
@@ -63,9 +63,9 @@ function Adminticket() {
       setLogin(window.localStorage.getItem('loggedin'))
       console.log(login)
      if(login==="false"){
-      router.push("/components/login/login")
+      Router.push("/components/login/login")
      } else if(login === null){
-      router.push("/components/login/login")
+        Router.push("/components/login/login")
      }
   
     })
