00938dc628361f44ab2d5f74c6a3f3b7de7ea909
admin <karthickdurai587@gmail.com>
2022-01-12 18:13:57 +0530

    all

--- pages/components/test.js
+++ pages/components/test.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import * as emailjs from "emailjs-com";
 import MailIcon from '@mui/icons-material/Mail';
-import * as emailjs from "emailjs-com";
+
 export default function Test() {
 
   return (
