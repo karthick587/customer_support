de577153982d53e52f42cb3f3c7f1d58f100dd17
muthu <muthu@sengen.com>
2022-01-15 20:47:44 +0530

    all

--- pages/components/tickets/teamticket.js
+++ pages/components/tickets/teamticket.js
@@ -5,10 +5,10 @@ import TableContainer from '@mui/material/TableContainer';
 import Paper from '@mui/material/Paper';
 import FormDialog from '../common/dialogsform';
 import { useRouter } from 'next/router'
-
+import * as emailjs from "emailjs-com";
 function Teamticket() {
     const Router = useRouter()
-
+    
     var [show, setShow] = useState('');
     var [tickets, setTickets] = useState([]);
 
@@ -26,11 +26,18 @@ function Teamticket() {
         setSelectedstatus(e.target.value)
     }
 
+    function updateemail(ticketsId,Username){
+        setName(Username);
+        setTicketid(ticketsId)
+    }
 
+    const [name,setName]=useState()
+  const [ticketid,setTicketid]=useState()
 
-
+ 
     function handleUpdatestatus(ticketsId) {
-
+            
+             
         console.log(ticketsId)
         console.log(statusUpdateTime)
 
@@ -42,10 +49,10 @@ function Teamticket() {
             setShow("update Successfully");
             Router.reload(window.location.pathname)
         });
-
-
+        //emailjs
+     
     }
-
+  
     var date, TimeType, hour, minutes, seconds, fullTime, dateupadate, monthupadate, yearupadate, fulldate;
     date = new Date();
     hour = date.getHours();
@@ -77,7 +84,7 @@ function Teamticket() {
     fulldate = dateupadate.toString() + '-' + monthupadate.toString() + '-' + yearupadate.toString()
 
     var [teamname, setTeamname] = useState(" ");
-
+    
     var [search1, setSearch1] = useState('');
     useEffect(() => {
         setSearch1(window.localStorage.getItem('tm_name'))
@@ -99,49 +106,153 @@ function Teamticket() {
         }
 
     })
+   
+    const[login,setLogin]=useState()
+  useEffect(()=>{
+    setLogin(window.localStorage.getItem('loggedin'))
+    console.log(login)
+   if(login==="false"){
+    router.push("/components/login/login")
+   } else if(login === null){
+    router.push("/components/login/login")
+   }
 
-    const [login, setLogin] = useState()
-    useEffect(() => {
-        setLogin(window.localStorage.getItem('loggedin'))
-        console.log(login)
-        if (login === "false") {
-            router.push("/components/login/login")
-        } else if (login === null) {
-            router.push("/components/login/login")
-        }
+  })
 
-    })
-
-
-
-
-
-
-
+  //to get client email id 
+  const [email,setEmail]=useState()
+  var [users, setUsers] = useState([]);
+  useEffect(() => {
+    Axios.get("https://mindmadetech.in/api/customer/list")
+        .then((res) => setUsers(res.data))  
+    }, []);
+useEffect(()=>{
+    {users.filter(val => {
+          
+        return  val.Username.toLowerCase().includes(name) 
+         
+   
+  }).map((itemed) =>setEmail(itemed.Email)
+  
+  )}
+})
+console.log(email)
+  //emailjs
+ 
+  
+  
+  
+  
+ 
     return (
         <div>
+          
+                <Head>
+                    <title>Admin Dashboard</title>
+                </Head>
 
-            <Head>
-                <title>Admin Dashboard</title>
-            </Head>
-
-            <div className="teambody">
-                <div className='adminticket-head'>
-                    <h1>Tickets</h1>
-                </div>
-                <TableContainer component={Paper}>
-                    <div className='tickets-bodyrow3'>
-                        <div >TicketId</div>
-                        <div>Username</div>
-                        <div >Date</div>
-                        <div>Team</div>
-                        <div>Status</div>
+                <div className="teambody">
+                    <div className='adminticket-head'>
+                        <h1>Tickets</h1>
                     </div>
-                 
-                </TableContainer>
-            </div>
+                    <TableContainer component={Paper}>
+                        <div className='tickets-bodyrow3'>
+                            <div >TicketId</div>
+                            <div>Username</div>
+                            <div >Date</div>
+                            <div>Team</div>
+                            <div>Status</div>
+                        </div>
+                        {tickets.filter(val => {
 
+                            return val.Team.toLowerCase().includes(teamname.toLowerCase())
+                             
+                        }).map((tickets) =>
+                            <div key={tickets.ticketsId} className='tickets-table-row3'>
 
+                                <FormDialog
+                                    dialogtitle={
+                                        <table  >
+                                            <tr className='tickets-bodyrow3' >
+                                                <td>{tickets.ticketsId}</td>
+                                                <td >{tickets.Username}</td>
+                                                <td>{tickets.Date}</td>
+                                                <td >{tickets.Team}</td>
+                                                <td >
+                                                    <h5 className={tickets.Status}>{tickets.Status}</h5>
+                                                    <h5 className='statusUpdateTime'>Updated at {tickets.statusUpdateTime}</h5>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    }
+                                    dialogbody={
+                                        <div className='ticket-details'>
+                                            <div className='ticket details-title'>Ticket NO {tickets.ticketsId}</div>
+                                            <div className='ticket details-name'>
+                                                <label className="label">Username</label>
+                                                <div className='ticket-input-details' >{tickets.Username}</div>
+                                            </div>
+                                            <div className='ticket details-no'>
+                                                <label className="label">Phonenumber</label>
+                                                <div className='ticket-input-details' >{tickets.Phonenumber}</div>
+                                            </div>
+                                            <div className='ticket details-domain'>
+                                                <label className="label">DomainName</label>
+                                                <div className='ticket-input-details' >{tickets.DomainName}</div>
+                                            </div>
+                                            <div className='ticket details-Date'>
+                                                <label className="label">Date</label>
+                                                <div className='ticket-input-details' > {tickets.Date}</div>
+                                            </div>
+                                            <div className='ticket details-Des'>
+                                                <label className="label">Description</label>
+                                                <div className='ticket-input-details' > {tickets.Description}</div>
+                                            </div>
+                                            <div className='ticket details-Status'><label className="label">Status</label>
+                                                <div  >Updated at {tickets.statusUpdateTime}</div>
+                                                <div className={tickets.Status} > {tickets.Status}</div>
+                                            </div>
+                                            <div className='ticket details-Team' ><label className="label">Team</label>
+                                                <div className='ticket-input-details' > {tickets.Team}</div></div>
+                                            <div className='ticket details-screenshots'><img src={tickets.screenshots} alt="screenshots" height="80vh" width="50%" /></div>
+                                        </div>
+                                    }
+                                />
+                                <FormDialog
+                                    dialogtitle={<div onClick={() => updateemail(tickets.ticketsId,tickets.Username)}>update</div>}
+                                    className="btn3 ticket-update2"
+                                    dialogbody={
+                                        <div className="form dialog" >
+                                            <div className="form-toggle"></div>
+                                            <div className="form-panel update one">
+                                                <div className="form-header">
+                                                    <h1>Update Ticket {tickets.ticketsId}</h1>
+                                                </div>
+                                                <div className="addform">
+                                                    <form>
+                                                        <div className="form-group">
+                                                            <label className="label">status</label>
+                                                            <select className="form-input" onChange={handlestatus}>
+                                                                <option value="">--Select Team--</option>
+                                                                <option className='new' value="new">new</option>
+                                                                <option className='inprogress' value="inprogress">inprogress</option>
+                                                                <option className='completed' value="completed">completed</option>
+                                                            </select>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                            <button className="btn2 float-end mt-3 mb-3" onClick={() => handleUpdatestatus(tickets.ticketsId)}>Assign</button>
+                                            <h4 className="alert1 text-center">{show}</h4>
+                                        </div>
+                                    }
+                                />
+                            </div>
+                        )}
+                    </TableContainer>
+                </div>
+
+           
         </div>
     );
 }
