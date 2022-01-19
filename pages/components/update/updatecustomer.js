89aba325ee1b10d7ed1c3314881ff6ab5452a739
Android-02\localadmin <madhupriya@mindmade.in>
2022-01-19 15:38:21 +0530

    update customer

--- pages/components/update/updatecustomer.js
+++ pages/components/update/updatecustomer.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState,createRef } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import Avatar from '@mui/material/Avatar';
 import FormDialog from '../common/dialogsform';
 import EditIcon from '@mui/icons-material/Edit';
-import { Formik, Form, Field } from 'formik';
+
 function Updatecustomer({ usersId }) {
     let router = useRouter();
     var [getCustomer, setGetCustomer] = useState([]);
@@ -13,28 +13,47 @@ function Updatecustomer({ usersId }) {
     const [selected, setSelected] = useState(false);
     var [show, setShow] = useState('');
     const [Adminname, setAdminname] = useState([])
+    let CompanynameR = createRef();
+    let ClientnameR = createRef();
+    let UsernameR = createRef();
+    let PasswordR = createRef();
+    let EmailR = createRef();
+    let PhonenumberR = createRef();
+
     useEffect(() => {
         setAdminname(window.localStorage.getItem('user'));
     }, [])
+    
     const [Modifiedby, setModifiedby] = useState()
     useEffect(() => {
         setModifiedby(Adminname.slice(3, 20));
-    })
+    },[])
     useEffect(() => {
         axios.get(`https://mindmadetech.in/api/customer/list/${usersId}`)
             .then(res => setGetCustomer(res.data))
-    }, [getCustomer])
-    function handleUpdate({ Companyname, Clientname, Username, Password, Email, Phonenumber }, Logo, usersId) {
-        var logo
+    }, [getCustomer]);
+
+    function handleUpdate(Logo,Companyname,Clientname, Username, Password, Email, Phonenumber) {
+
+        var Logo,Companyname,Clientname,Username,Password,Email,Phonenumber;
+        Companyname = CompanynameR.current.value;
+        Clientname = ClientnameR.current.value;
+        Username = UsernameR.current.value;
+        Password = PasswordR.current.value;
+        Email = EmailR.current.value;
+        Phonenumber = PhonenumberR.current.value;
+        console.log(Companyname,Clientname,Username,Password,Email,Phonenumber)
         switch (editLogo) {
             case undefined:
-                logo = Logo;
+                Logo = Logo;
+                console.log("empty");
                 break;
             default:
-                logo = editLogo;
+                Logo = editLogo;
+                console.log("editLogo");
                 break;
         }
-
+        console.log(Logo);
         const data = new FormData();
         data.append("Companyname", Companyname);
         data.append("Clientname", Clientname);
@@ -42,19 +61,25 @@ function Updatecustomer({ usersId }) {
         data.append("Phonenumber", Phonenumber);
         data.append("Username", Username);
         data.append("Password", Password);
-        data.append("file", logo);
-        data.append("Modifiedon", date + ' ' + fullTime);
-        data.append("Modifiedby", Modifiedby);
-        axios.put(`https://mindmadetech.in/api/customer/update/${usersId}`, data, {
+        if(editLogo){
+            data.append("file", Logo);
+        }else {
+            data.append("Logo",Logo)
+        }
+        data.append("Modifiedon",date + ' ' + fullTime);
+        data.append("Modifiedby",Modifiedby)
+    
+        axios.put(`https://mindmadetech.in/api/customer/update/${usersId}`,data,{
             headers: {
                 'Content-Type': 'multipart/form-data',
             }
         }).then((res) => {
-            setShow("Updated Successfully")
-            localStorage.setItem('updateclose', "close");
-        })
-    }
-
+                setShow("Updated Successfully")
+                localStorage.setItem('updateclose', "close");
+            })
+           
+        }
+   
     setTimeout(() => {
         setShow()
     }, [3500])
@@ -98,55 +123,59 @@ function Updatecustomer({ usersId }) {
             dialogbody={
                 <div>
                     {getCustomer.map((data) =>
-                        <div className="container dialog-body" key={data.usersId}>
-                            <Formik
-                                className="addform"
-                                initialValues={{ Logo: data.Logo, Companyname: data.Companyname, Clientname: data.Clientname, Username: data.Username, Password: data.Password, Email: data.Email, Phonenumber: data.Phonenumber }}
-                                onSubmit={value => handleUpdate(value, data.Logo, data.usersId)}
-                            >
-                                <Form >
-                                    <div className="form-group upload">
-                                        <label htmlFor="contained-button-file">
-                                            <input accept="image/*" id="contained-button-file" className="upload-input-button" multiple type="file" onChange={(e) => handleScreenshot(e)} />
-                                            <Avatar
-                                                alt="Remy Sharp"
-                                                src={selected === false ? data.Logo : uploadLogo}
-                                                sx={{ width: 65, height: 65 }}
-                                            />
-                                        </label>
+                        <div className="container mainbody" key={data.usersId}>
+                            <div className="addform">
+                                <form>
+                                <div className="form-group upload">
+                                    <label htmlFor="contained-button-file">
+                                        <input accept="image/*" id="contained-button-file" className="upload-input-button" multiple type="file" onChange={(e)=>handleScreenshot(e)} />
+                                        <Avatar
+                                            alt="Remy Sharp"
+                                            src = {selected === false ? data.Logo : uploadLogo}
+                                            sx={{ width: 65, height: 65 }}
+                                        />
+                                    </label>
+                                </div>
+
+                                <div className="form-group">
+                                        <label className="label"> Comapany Name</label>
+                                        <input className="form-input" name="Name" type="text" ref={CompanynameR} defaultValue={data.Companyname}  />
+
                                     </div>
                                     <div className="form-group">
-                                        <label className="label">Companyname</label>
-                                        <Field className="form-input" name="Companyname" />
+                                        <label className="label"> Client Name</label>
+                                        <input className="form-input" name="Name" type="text" ref={ClientnameR} defaultValue={data.Clientname}  />
+
                                     </div>
                                     <div className="form-group">
-                                        <label className="label">Clientname</label>
-                                        <Field className="form-input" name="Clientname" />
-                                    </div>
-                                    <div className="form-group">
-                                        <label className="col label">Username</label>
-                                        <Field className="form-input" name="Username" />
+                                        <label className="label">Username</label>
+                                        <input className="form-input" name="Username" type="text" ref={UsernameR} defaultValue={data.Username}  />
+
                                     </div>
                                     <div className="form-group">
                                         <label className="col label">Password</label>
-                                        <Field className="form-input" name="Password" />
+                                        <input className="form-input" name="Password" type="text" ref={PasswordR} defaultValue={data.Password}  />
+
                                     </div>
                                     <div className="form-group">
-                                        <label className="col label">Email</label>
-                                        <Field className="form-input" name="Email" />
+                                        <label className="col label">EMail ID</label>
+                                        <input className="form-input" name="Email" type="text" ref={EmailR} defaultValue={data.Email}  />
+
                                     </div>
                                     <div className="form-group">
-                                        <label className="col label">Phonenumber</label>
-                                        <Field className="form-input" name="Phonenumber" />
+                                        <label className="col label">Phone Number</label>
+                                        <input className="form-input" name="Phonenumber" type="text" ref={PhonenumberR} defaultValue={data.Phonenumber}  />
+
                                     </div>
                                     <div className="row justify-content-center">
                                         <div className='bottom-area'>
-                                            <button type="submit" className="btn2 float-end">Submit</button>
+                                            <button type="button" onClick={() => handleUpdate(data.Logo,data.Companyname,data.Clientname, data.Username, data.Password, data.Email, data.Phonenumber)} className="btn2 float-end"> Update </button>
                                         </div>
                                     </div>
                                     <h3>{show}</h3>
-                                </Form>
-                            </Formik>
+                                </form>
+
+                            </div>
                         </div>
                     )}
                 </div>}
