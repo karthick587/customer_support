396a62f325e75ca44d57b53d82a62df64a496997
admin <karthickdurai587@gmail.com>
2022-02-01 17:37:24 +0530

    all

--- pages/components/tickets/teamticket.js
+++ pages/components/tickets/teamticket.js
@@ -13,7 +13,8 @@ import TableRow from '@mui/material/TableRow';
 import Ticketviewer from '../common/ticketviewer';
 import { CounterContext } from '../contex/adminProvider'
 function Teamticket(props) {
-    const {teamticket}=props
+    const { teamticket } = useContext(CounterContext);
+   
     const Router = useRouter()
     var [show, setShow] = useState('');
     
