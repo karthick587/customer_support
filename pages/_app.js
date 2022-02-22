d4a5b05c1a39bbdb46e96344f4eccaeb6acc9ddb
admin <karthickdurai587@gmail.com>
2022-02-22 15:37:25 +0530

    al

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import '../styles/style.css';
+import Head from 'next/head';
 import '../styles/mainpage.css';
 import { useEffect } from "react";
-import Head from 'next/head';
 import 'keen-slider/keen-slider.min.css';
 import NProgress from 'nprogress'; //nprogress module
 import 'nprogress/nprogress.css'; //styles of nprogress
 import Router from 'next/router';
 import CounterContextProvider from './components/contex/adminProvider';
-
 function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
@@ -20,15 +19,21 @@ function MyApp({ Component, pageProps }) {
   Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done());
   NProgress.configure({ showSpinner: false });
 
-  return <CounterContextProvider>
-    <Head>
-
-      {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" /> */}
-
-    </Head>
+  return (
+    <>
+            {/* Add the favicon */}
+      <Head>
+        <link rel="shortcut icon" href="../public/fav.png" />
+        <title>Customer Support</title>
+      </Head>
+      {/* Add the favicon */}
+              
+  <CounterContextProvider>
+    
     <Component {...pageProps} />
     </CounterContextProvider>
+    </>
+  )
 }
 
 export default MyApp
