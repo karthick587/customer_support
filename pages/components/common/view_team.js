c16c49d695011b23ee15210df0f2c32b21a4c51c
muthu <muthu@sengen.com>
2022-02-27 00:13:52 +0530

    all

--- pages/components/common/view_team.js
+++ pages/components/common/view_team.js
@@ -3,24 +3,24 @@ import React, { useContext,useState,useEffect } from "react";
 import Axios from "axios";
 export default function ViewTeam(props){
     const {teamArray,team}=props
-    const [teams,setteams]=useState([])
+    
     const [newarray,setnewarray]=useState([])
     useEffect(()=>{
-        setteams(team)
+       
         setnewarray(teamArray.map((product)=>[...newarray,product.teamId] )) 
     },[setnewarray,teamArray])
   
     return(
-        <div className="">
-        {teams.filter(val => { 
-             for (let i = 0; i <= 20; i++) {
-            return val.teamId.toString().includes(newarray[i])
-             } 
-         }).map((product) =>
-                                 <div className=" " key={product.teamId}>
-                                     <div>{product.Username},</div>
-                                </div>
-                             )}
+        <div className="">{team!==undefined ?<> {team.filter(val => { 
+            for (let i = 0; i <= 20; i++) {
+           return val.teamId.toString().includes(newarray[i])
+            } 
+        }).map((product) =>
+                                <div className=" " key={product.teamId}>
+                                    <div>{product.Username},</div>
+                               </div>
+                            )}</>:<></>}
+       
      </div>
     )
 }
