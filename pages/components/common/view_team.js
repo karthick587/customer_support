c4eca67e632f9ca9a9de6013ab6485a4b4931e60
muthu <muthu@sengen.com>
2022-02-27 00:07:02 +0530

    all

--- pages/components/common/view_team.js
+++ pages/components/common/view_team.js
@@ -3,9 +3,10 @@ import React, { useContext,useState,useEffect } from "react";
 import Axios from "axios";
 export default function ViewTeam(props){
     const {teamArray,team}=props
-    const [teams,setteams]=useState(team)
+    const [teams,setteams]=useState([])
     const [newarray,setnewarray]=useState([])
     useEffect(()=>{
+        setteams(team)
         setnewarray(teamArray.map((product)=>[...newarray,product.teamId] )) 
     },[setnewarray,teamArray])
   
