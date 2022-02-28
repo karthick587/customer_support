6483ba76c743010e7d5046128816b517d5da59e4
admin <karthickdurai587@gmail.com>
2022-02-28 16:35:59 +0530

    all

--- pages/components/common/non_userviewer.js
+++ pages/components/common/non_userviewer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useContext } from 'react';
 import Imageviewer from '../common/imageviewer'
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import Axios from "axios";
