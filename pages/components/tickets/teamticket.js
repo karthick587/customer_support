765ba4a0ceebb93df0570c1f4ed192b395be79a9
admin <karthickdurai587@gmail.com>
2022-03-08 17:32:30 +0530

    all

--- pages/components/tickets/teamticket.js
+++ pages/components/tickets/teamticket.js
@@ -19,11 +19,10 @@ import ReactPaginate from 'react-paginate';
 import { Typography } from '@mui/material';
 
 function Teamticket(props) {
-    const {teamassignedcount}=props
     const { team } = useContext(ListContext);
-    const { setdialogformopen, setTesting, setshowvalue } = useContext(CounterContext);
+    const { setdialogformopen,setTesting,setshowvalue } = useContext(CounterContext);
     const { currentDate } = useContext(CurrentDateContext);
-    const { teamticket, loginTmName } = props;
+    const { teamticket,loginTmName } = props;
     const [mapteamticket, setmapteamticket] = useState([]);
     const Router = useRouter();
     var [selectedstatus, setSelectedstatus] = useState('');
@@ -36,7 +35,7 @@ function Teamticket(props) {
     function handlestatus(e) {
         setSelectedstatus(e.target.value);
     };
-
+    
     //tickets status update functions 
     const updateemail = (Status) => {
         if (Status === "Completed") {
@@ -45,29 +44,29 @@ function Teamticket(props) {
             setdisabled("enable");
         }
     };
-
+   
     //status submit function
-    function handleUpdatestatus(ticketsId, TeamAssign) {
-        var teamId = team.filter(Id => {
-            if (Id.Username.includes(loginTmName)) {
-                return Id;
-            }
-        }).map(team => {
+    function handleUpdatestatus(ticketsId,TeamAssign) {
+       var teamId = team.filter(Id=>{
+           if(Id.Username.includes(loginTmName)){
+               return Id;
+           }
+        }).map(team=>{
             return team.teamId
         });
-        var tickets_assignId = TeamAssign.filter(id => {
-            if (id.teamId.toString().includes(teamId[0].toString())) {
-                return id;
-            }
-        }).map(assign => {
+       var tickets_assignId = TeamAssign.filter(id=>{
+           if(id.teamId.toString().includes(teamId[0].toString())){
+               return id;
+           }
+        }).map(assign=>{
             return assign.tickets_assignId;
         })
-
+        
         if (selectedstatus === 'started') {
             Axios.put(`https://mindmadetech.in/api/tickets/status/update`, {
                 Status: selectedstatus,
                 ticketsId: ticketsId,
-                tickets_assignId: tickets_assignId[0],
+                tickets_assignId : tickets_assignId[0],
                 Tm_Start_UpdatedOn: currentDate,
                 Tm_Start_UpdatedBy: window.localStorage.getItem('tm_name')
             }).then((response) => {
@@ -77,14 +76,13 @@ function Teamticket(props) {
                 setshowvalue("Registered Successfully");
             }).catch((err) => {
                 setTesting(true)
-                setshowvalue(1 + "Registered failed");
-                return err;
-            })
+                setshowvalue(1+"Registered failed");
+                return err; })
         } else if (selectedstatus === 'inprogress') {
             Axios.put(`https://mindmadetech.in/api/tickets/status/update`, {
                 Status: selectedstatus,
                 ticketsId: ticketsId,
-                tickets_assignId: tickets_assignId[0],
+                tickets_assignId : tickets_assignId[0],
                 Tm_Process_UpdatedOn: currentDate,
                 Tm_Process_UpdatedBy: window.localStorage.getItem('tm_name')
             }).then((response) => {
@@ -92,16 +90,15 @@ function Teamticket(props) {
                 localStorage.setItem("passValue", true);
                 setTesting(true)
                 setshowvalue("Updated Successfully");
-            }).catch((err) => {
+            }).catch((err) => { 
                 setTesting(true)
-                setshowvalue(1 + "Registered failed");
-                return err;
-            })
+                setshowvalue(1+"Registered failed");
+                return err; })
         } else if (selectedstatus === 'completed') {
             Axios.put(`https://mindmadetech.in/api/tickets/status/update`, {
                 Status: selectedstatus,
                 ticketsId: ticketsId,
-                tickets_assignId: tickets_assignId[0],
+                tickets_assignId : tickets_assignId[0],
                 Tm_Complete_UpdatedOn: currentDate,
                 Tm_Complete_UpdatedBy: window.localStorage.getItem('tm_name')
             }).then((response) => {
@@ -109,18 +106,17 @@ function Teamticket(props) {
                 localStorage.setItem("passValue", true);
                 setTesting(true)
                 setshowvalue("Updated Successfully");
-            }).catch((err) => {
+            }).catch((err) => { 
                 setTesting(true)
-                setshowvalue(1 + "Registered failed");
-                return err;
-            })
+                setshowvalue(1+"Registered failed");
+                return err; })
         } else return null
     };
 
 
     //auth access for team ticket page
     useEffect(() => {
-
+      
         setmapteamticket(teamticket.reverse());
         setLogin(window.localStorage.getItem('loggedin'));
         if (login === "false") {
@@ -137,7 +133,7 @@ function Teamticket(props) {
     function closeDetails() {
         setShowdetails(false);
     };
-
+   
     const [datalimit, setdatalimit] = useState(10);
     const [currentpage, setCurrentpage] = useState(1);
     //pagination
@@ -155,7 +151,7 @@ function Teamticket(props) {
             </Head>
             {showdetails === false ?
                 <div className="teambody">
-                    <div className='dash-head mt-1 mb-1'>
+                   <div className='dash-head mt-1 mb-1'>
                         <h1>TICKETS</h1>
                     </div>
                     <TableContainer component={Paper}>
@@ -169,81 +165,78 @@ function Teamticket(props) {
                                     <TableCell align="left">STATUS</TableCell>
                                 </TableRow>
                             </TableHead>
-                            {teamassignedcount === 0 ? <div className='text-center'>No Tickets Has been Assigned</div> : <div>
-                                {mapteamticket.slice((currentpage - 1) * datalimit, currentpage * datalimit).map((tickets) =>
-                                    <TableBody key={tickets.ticketsId} className='update-right' >
-                                        <TableRow className="tickets-bodyrow update6" onClick={() => Notificationupdate(tickets.ticketsId, tickets.Screenshots)}>
-                                            <TableCell>{tickets.ticketsId}</TableCell>
-                                            <TableCell >{tickets.Username}</TableCell>
-                                            <TableCell >{tickets.Cus_CreatedOn === null ? <>{tickets.Adm_CreatedOn}</> : <>{tickets.Cus_CreatedOn}</>}</TableCell>
-                                            <TableCell >
-                                                <ViewTeam teamArray={tickets.TeamAssign} />
-                                            </TableCell>
-                                            <TableCell > {tickets.Status === "completed" ? <h5 className={tickets.Status}>Done</h5> : <h5 className={tickets.Status}>{tickets.Status}</h5>}
-                                            </TableCell>
-                                        </TableRow>
-                                        <FormDialog
-                                            dialog_className="update7"
-                                            dialogtitle={<div onClick={() => updateemail(tickets.Status)}>update</div>}
-                                            className="btn3 ticket-update2"
-                                            dialogbody={<div>{disabled === "disabled" ? <div className='ticket-update-alert'>Ticket has been completed</div> :
-                                                <div className="form dialog" >
-                                                    <div className="form-toggle"></div>
-                                                    <div className="form-panel update one">
-                                                        <div className="form-header">
-                                                            <h1>Update Ticket {tickets.ticketsId}</h1>
-                                                        </div>
-                                                        <div className="addform">
-                                                            <form>
-                                                                <div className="form-group">
-                                                                    <label className="label">Status</label>
-                                                                    <select className="form-input" onChange={handlestatus}>
-                                                                        <option value="">--Select Status--</option>
-                                                                        <option className='started' value="started">started</option>
-                                                                        <option className='inprogress' value="inprogress">inprogress</option>
-                                                                        <option className='completed' value="completed">completed</option>
-                                                                    </select>
-                                                                </div>
-                                                            </form>
-                                                        </div>
+                            {mapteamticket.slice((currentpage - 1) * datalimit, currentpage * datalimit).map((tickets) =>
+                                <TableBody key={tickets.ticketsId} className='update-right' >
+                                    <TableRow className="tickets-bodyrow update6" onClick={() => Notificationupdate(tickets.ticketsId, tickets.Screenshots)}>
+                                        <TableCell>{tickets.ticketsId}</TableCell>
+                                        <TableCell >{tickets.Username}</TableCell>
+                                        <TableCell >{tickets.Cus_CreatedOn===null ? <>{tickets.Adm_CreatedOn}</>:<>{tickets.Cus_CreatedOn}</> }</TableCell>
+                                        <TableCell >
+                                        <ViewTeam  teamArray={tickets.TeamAssign}  />
+                                        </TableCell>
+                                        <TableCell > {tickets.Status === "completed" ? <h5 className={tickets.Status}>Done</h5> : <h5 className={tickets.Status}>{tickets.Status}</h5>}
+                                        </TableCell>
+                                    </TableRow>
+                                    <FormDialog
+                                        dialog_className="update7"
+                                        dialogtitle={<div onClick={() => updateemail(tickets.Status)}>update</div>}
+                                        className="btn3 ticket-update2"
+                                        dialogbody={<div>{disabled === "disabled" ? <div className='ticket-update-alert'>Ticket has been completed</div> :
+                                            <div className="form dialog" >
+                                                <div className="form-toggle"></div>
+                                                <div className="form-panel update one">
+                                                    <div className="form-header">
+                                                        <h1>Update Ticket {tickets.ticketsId}</h1>
                                                     </div>
-                                                    <button className="btn2 float-end mt-3 mb-3" onClick={() => handleUpdatestatus(tickets.ticketsId, tickets.TeamAssign)}>update</button>
+                                                    <div className="addform">
+                                                        <form>
+                                                            <div className="form-group">
+                                                                <label className="label">Status</label>
+                                                                <select className="form-input" onChange={handlestatus}>
+                                                                    <option value="">--Select Status--</option>
+                                                                    <option className='started' value="started">started</option>
+                                                                    <option className='inprogress' value="inprogress">inprogress</option>
+                                                                    <option className='completed' value="completed">completed</option>
+                                                                </select>
+                                                            </div>
+                                                        </form>
+                                                    </div>
                                                 </div>
-                                            }
+                                                <button className="btn2 float-end mt-3 mb-3" onClick={() => handleUpdatestatus(tickets.ticketsId,tickets.TeamAssign)}>update</button>                                  
                                             </div>
-                                            }
-                                        />
-                                    </TableBody>
-                                )}
-                            </div>}
-
+                                        }
+                                        </div>
+                                        }
+                                    />
+                                </TableBody>
+                            )}
                         </Table>
                     </TableContainer>
-                    {mapteamticket.length < 10 ? <></> :
-                        <div className='page-bottom'>
-                            < ReactPaginate
-                                previousLabel={""}
-                                nextLabel={""}
-                                pageCount={Math.ceil(mapteamticket.length / datalimit)}
-                                onPageChange={(e) => handlePageChange(e.selected)}
-                                containerClassName={"pagination mt-3"}
-                                pageClassName={"page-item"}
-                                pageLinkClassName={"page-link"}
-                                activeClassName={"active"}
-                            />
-                            <div className='pagedata-limit flex'>
-                                <Typography>Team per page</Typography>
+                    {mapteamticket.length<10 ? <></>:
+                     <div className='page-bottom'>
+                     < ReactPaginate
+                         previousLabel={""}
+                         nextLabel={""}
+                         pageCount={Math.ceil(mapteamticket.length / datalimit)}
+                         onPageChange={(e) => handlePageChange(e.selected)}
+                         containerClassName={"pagination mt-3"}
+                         pageClassName={"page-item"}
+                         pageLinkClassName={"page-link"}
+                         activeClassName={"active"}
+                     />
+                     <div className='pagedata-limit flex'>
+                         <Typography>Team per page</Typography>
 
-                                <select className='pagedatalimit-select' onChange={pagedatalimit}>
-                                    <option value={10}>10</option>
-                                    <option value={25}>25</option>
-                                    <option value={50}>50</option>
-                                    <option value={100}>100</option>
-                                </select>
-                            </div>
-                        </div>
+                         <select className='pagedatalimit-select' onChange={pagedatalimit}>
+                             <option value={10}>10</option>
+                             <option value={25}>25</option>
+                             <option value={50}>50</option>
+                             <option value={100}>100</option>
+                         </select>
+                     </div>
+                 </div>
                     }
-
+                   
                 </div>
                 :
                 <>
